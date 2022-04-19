a11b0ef11af3dfe53ebebe7e4d0b05c2ccdaaff5
ibandominguez <domingueznoda@gmail.com>
2022-04-19 11:25:25 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Qemu-vms
+
+Collection of qemu scripts to provision, install and run vms.
+
+## Dependencies
+
+```sh
+# MacOs
+brew install qemu
+
+# Linux / Debian
+sudo apt-get install qemu-kvm qemu virt-manager virt-viewer libvirt-bin
+```
+
+## TODO
+
+- [ ] Add documentation
+- [ ] Add Rpi example
---
+++ ubuntu-12.04.5-x86/start.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# Change working directory
+cd "$(dirname "$0")"
+
+# Parameters.
+version=12.04
+id=ubuntu-12.04.5-desktop-i386
+disk_img="${id}.img.qcow2"
+disk_img_snapshot="${id}.snapshot.qcow2"
+iso="${id}.iso"
+
+# Get image.
+if [ ! -f "$iso" ]; then
+  wget "http://releases.ubuntu.com/${version}/${iso}"
+fi
+
+# Go through installer manually.
+if [ ! -f "$disk_img" ]; then
+  qemu-img create -f qcow2 "$disk_img" 5G
+  qemu-system-x86_64 \
+    -cdrom "$iso" \
+    -drive "file=${disk_img},format=qcow2" \
+    -m 2G \
+    -smp 2 \
+  ;
+fi
+
+# Create an image based on the original post-installation image
+# so as to keep a pristine post-install image.
+if [ ! -f "$disk_img_snapshot" ]; then
+  qemu-img \
+    create \
+    -f qcow2 \
+    -F qcow2 \
+    -b "$disk_img" \
+    "$disk_img_snapshot" \
+  ;
+fi
+
+# Run the copy of the installed image.
+qemu-system-x86_64 \
+  -drive "file=${disk_img_snapshot},format=qcow2" \
+  -m 4G \
+  -smp 2 \
+  -soundhw hda \
+  -vga virtio \
+  -accel hvf \
+  "$@" \
+;
