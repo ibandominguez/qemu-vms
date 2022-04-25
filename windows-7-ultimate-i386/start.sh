784b77814ab9664b454913765b2528f978927c96
ibandominguez <domingueznoda@gmail.com>
2022-04-25 16:29:58 +0800

    Added win732bits folder

---
+++ windows-7-ultimate-i386/start.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# Change working directory
+cd "$(dirname "$0")"
+
+# Parameters.
+id=windows-7-ultimate-i386
+disk_img="${id}.img.qcow2"
+disk_img_snapshot="${id}.snapshot.qcow2"
+iso="${id}.iso"
+
+# Get image.
+if [ ! -f "$iso" ]; then
+  echo "ISO $iso not found"
+  exit
+fi
+
+# Go through installer manually.
+if [ ! -f "$disk_img" ]; then
+  qemu-img create -f qcow2 "$disk_img" 15G
+  qemu-system-x86_64 \
+    -cdrom "$iso" \
+    -drive "file=${disk_img},format=qcow2" \
+    -m 4G \
+    -accel hvf \
+    -smp 2 \
+  ;
+fi
+
+# Create an image based on the original post-installation image
+# so as to keep a pristine post-install image.
+if [ ! -f "$disk_img_snapshot" ]; then
+  qemu-img \
+    create \
+    -f qcow2 \
+    -F qcow2 \
+    -b "$disk_img" \
+    "$disk_img_snapshot" \
+  ;
+fi
+
+# Run the copy of the installed image.
+qemu-system-x86_64 \
+  -drive "file=${disk_img_snapshot},format=qcow2" \
+  -m 4G \
+  -smp 2 \
+  -accel hvf \
+  "$@" \
+;
